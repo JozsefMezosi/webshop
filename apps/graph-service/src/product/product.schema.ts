24d5e435166cd6d068d0b86b96759ad3afcef9c2
Jozsef Mezosi <mezosijoci@gmail.com>
2023-12-29 16:18:08 +0100

    add createProduct mutation

--- apps/graph-service/src/product/product.schema.ts
+++ apps/graph-service/src/product/product.schema.ts
@@ -1,12 +1,16 @@
 export const productTypeDefs = `#graphql
    type Product @authorization(validate: [
-    { where: { jwt: { roles_INCLUDES: "admin" } }, operations: [CREATE, UPDATE, DELETE]}
-]){
+    { where: { jwt: { roles_INCLUDES: "admin" } }, operations: [DELETE]}
+]) @mutation(operations: [DELETE]){
     id: ID! @id
     name: String! @unique(constraintName: "unique_product")
     description: String!
     priceInEuro: Float!
     details: Object
     imageUrls: [String!]!
+   }
+
+   type Mutation {
+        createProduct(input: ProductCreateInput!): Product!
    }
 `;
