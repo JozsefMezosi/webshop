7171d7cd36117c02cac7b85bdc8aecd7ba733fc9
Jozsef Mezosi <mezosijoci@gmail.com>
2024-01-04 18:34:42 +0100

    fix navbar z-index

--- apps/web/components/navbar/navbar.tsx
+++ apps/web/components/navbar/navbar.tsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
   }
 
   return (
-    <nav className="px-10 py-3 shadow-sm shadow-gray-400 fixed w-screen overflow-hidden bg-white flex justify-between items-center">
+    <nav className="px-10 z-50 py-3 shadow-sm shadow-gray-400 fixed w-screen overflow-hidden bg-white flex justify-between items-center">
       <div>Navbar</div>
       <NavbarRightSide />
     </nav>
